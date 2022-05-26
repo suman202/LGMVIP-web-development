99d101cea3934d655a5bccecb2d7ce8ab8827e04
suman202 <73178365+suman202@users.noreply.github.com>
2022-05-26 06:15:47 -0700

    Add files via upload

---
+++ Calculator/Title.js
@@ -0,0 +1,10 @@
+// Title component
+export default () => {
+  return (
+    <div className="title">
+      <span className="caption">
+         <strong>Calculator</strong>
+      </span>
+    </div>
+  )
+}
